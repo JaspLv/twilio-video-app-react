9cbe27d010ba77366903effa05ee7eec2b458f29
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2025-11-13 21:25:52 +0100

    test: AudioContext

--- src/components/AudioTrack/AudioTrack.tsx
+++ src/components/AudioTrack/AudioTrack.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { AudioTrack as IAudioTrack } from 'twilio-video';
 import { useAppState } from '../../state';
+import RemoteAudio from './RemoteAudio';
 
 interface AudioTrackProps {
   track: IAudioTrack;
@@ -9,12 +10,29 @@ interface AudioTrackProps {
 export default function AudioTrack({ track }: AudioTrackProps) {
   const { activeSinkId } = useAppState();
   const audioEl = useRef<HTMLAudioElement>();
+  const audioCtx = useRef<AudioContext>();
+  const remoteAudio = useRef<RemoteAudio>();
 
   useEffect(() => {
     audioEl.current = track.attach();
     audioEl.current.setAttribute('data-cy-audio-track-name', track.name);
+    audioEl.current.muted = true;
+
     document.body.appendChild(audioEl.current);
-    return () =>
+
+    audioCtx.current = new AudioContext();
+
+    const mediaStream = new MediaStream([track.mediaStreamTrack]);
+
+    remoteAudio.current = new RemoteAudio(audioCtx.current, { mediaStream });
+    remoteAudio.current.connect(audioCtx.current.destination);
+
+    return () => {
+      remoteAudio.current?.destroy();
+      remoteAudio.current = undefined;
+      audioCtx.current?.close();
+      audioCtx.current = undefined;
+
       track.detach().forEach(el => {
         el.remove();
 
@@ -22,6 +40,7 @@ export default function AudioTrack({ track }: AudioTrackProps) {
         // See: https://github.com/twilio/twilio-video.js/issues/1528
         el.srcObject = null;
       });
+    };
   }, [track]);
 
   useEffect(() => {
